0f28eb72ceb0bb2e4c2ac2d181e97248c8d1ea6b
Evil_Musey <cosey2yhn@gmail.com>
2017-06-25 22:11:46 +0800

    Add files via upload
    根据输入的参数值，拼接 SQL 查询语句并执行，将查询结果展示。如根据输入的学号展示姓名和分数。

---
+++ 1.php
@@ -0,0 +1,48 @@
+<?php 
+
+$con=mysqli_connect("localhost","root","123qwe");
+
+if (!$con) {	
+	die ('connect error:'.mysql_error());
+}
+
+$db_selectd=mysql_select_db("test",$con);
+
+if(isset($_GET['id'])){
+
+	$id=$_GET['id'];
+	$sql="SELECT * FROM student WHERE id=".$id;
+
+	$result=mysql_query($sql,$con);
+
+	if ($row=mysql_fetch_assoc($result)) {		
+		echo "id:".$row["id"]."</br>";
+		echo "name:".$row["name"]."</br>";
+		echo "score:".$row["score"]."</br>";
+	}
+	else(
+		echo "without this id";
+	)
+}
+else{
+	echo "get id error";
+}
+
+mysql_close($con);
+ 
+ ) 
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
