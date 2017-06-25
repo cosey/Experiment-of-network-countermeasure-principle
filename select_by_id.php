2e9bc1d281814ee354a0262e9741ff0df78299c3
Evil_Musey <cosey2yhn@gmail.com>
2017-06-26 00:27:12 +0800

    Update select_by_id.php

--- select_by_id.php
+++ select_by_id.php
@@ -2,10 +2,19 @@
 
 $dbhost = 'localhost';
 $dbusername = 'root';
-$dbpassword = '123qwe';
+$dbpassword = '';
 $dbname = 'test';
-$db = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
 
+if (!$_GET){
+	echo 'Please input the student id in the url (?id=xxx).';
+	exit;
+}else{
+	$db = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
+
+	if (mysqli_connect_errno()){
+		echo 'Can not connect the database.';
+		exit;
+	}
 	
 	$query = "SELECT * FROM student WHERE id=".$_GET['id'];
 	// var_dump($query);
@@ -15,6 +24,6 @@ $db = mysqli_connect($dbhost, $dbusername, $dbpassword, $dbname);
 		echo 'name: '.$row['name'].'<br />';
 		echo 'score: '.$row['score'].'<br />';
 	}
-
+}
 
 ?>
